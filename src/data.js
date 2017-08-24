854fda1e56aff179253a331476e6cf627904a2f8
Glen Maddern <glenmaddern@gmail.com>
2017-08-24 17:15:20 +1000

    logos in, roughly

--- src/data.js
+++ src/data.js
@@ -1,5 +1,10 @@
+import js_logo from './js/logo.svg'
+import css_logo from './css/logo.svg'
+import decompress_logo from './decompress/logo.svg'
+
 export const JS = {
   title: 'JSConf AU 2018',
+  logo: js_logo,
   theme: {
     primary: 'blue'
   }
@@ -7,6 +12,7 @@ export const JS = {
 
 export const CSS = {
   title: 'CSSConf AU 2018',
+  logo: css_logo,
   theme: {
     primary: 'orange'
   }
@@ -14,6 +20,7 @@ export const CSS = {
 
 export const Decompress = {
   title: 'Decompress 2018',
+  logo: decompress_logo,
   theme: {
     primary: 'red'
   }
--- src/shared/Home.js
+++ src/shared/Home.js
@@ -8,13 +8,20 @@ const Frame = styled.div`
   align-items: center;
   min-height: 100vh;
   
-  color: ${ props => props.theme.primary }
+  background: ${ props => props.theme.primary };
+  color: white;
+  
+  > img {
+    max-width: calc(100vw - 4rem);
+    max-height: 80vh;
+  }
 `
 
 export default ({ conference }) => (
   <ThemeProvider theme={ conference.theme }>
     <Frame>
       <h1>HI EVERYONE WELCOME TO {conference.title}!</h1>
+      <img src={conference.logo} alt="Logo"/>
     </Frame>
   </ThemeProvider>
 )
