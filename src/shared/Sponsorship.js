5a3a393b7fba1c29bd642831c128d948a0b6e917
Glen Maddern <glenmaddern@gmail.com>
2017-08-25 16:00:55 +1000

    and one more

--- src/shared/Sponsorship.js
+++ src/shared/Sponsorship.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Headline from './components/Headline'
+import Copy from './components/Copy'
 
 export default ({ sponsorship }) => {
   return (
     <div>
       <Headline>Sponsorship</Headline>
 
-      <p>We’re looking for more people to help support this event.
-      If your organisation could help, please drop an email to <a href={`mailto:${sponsorship}`}>{sponsorship}</a> for more details.</p>
+      <Copy>We’re looking for more people to help support this event.
+      If your organisation could help, please drop an email to <a href={`mailto:${sponsorship}`}>{sponsorship}</a> for more details.</Copy>
     </div>
   )
 }
