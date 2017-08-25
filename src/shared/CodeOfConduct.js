58511c58fad8522a7ce5ec380c77ccae14cbc698
Glen Maddern <glenmaddern@gmail.com>
2017-08-25 15:21:48 +1000

    router hax

--- src/shared/CodeOfConduct.js
+++ src/shared/CodeOfConduct.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const CodeOfConduct = styled.div`
   margin: 20px auto;
@@ -10,7 +11,7 @@ const CodeOfConduct = styled.div`
 export default () => {
   return (
     <CodeOfConduct>
-      <p>All organisers, sponsors, speakers and attendees are required to follow the <a href="/codeofconduct">conference Code of Conduct</a>.
+      <p>All organisers, sponsors, speakers and attendees are required to follow the <Link to="codeofconduct">conference Code of Conduct</Link>.
       We are committed to providing a harassment-free, professional and welcoming environment for all attendees.</p>
     </CodeOfConduct>
   )
--- src/shared/Home.js
+++ src/shared/Home.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
+import { Route, withRouter } from 'react-router-dom'
+import CodeOfConductFull from './CodeOfConductFull'
 
-const Frame = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+const Background = styled.div`
   min-height: 100vh;
 
   background: ${ props => props.theme.primary};
   color: white;
 `
 
-export default ({ conference, children }) => (
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 70rem;
+  padding: 1rem;
+`
+
+export default withRouter(({ conference, children, match }) => (
   <ThemeProvider theme={conference.theme}>
-    <Frame>
-      {children}
-    </Frame>
+    <Background>
+      <Route exact path={`${match.url}`} render={() => <Container>{children}</Container>}/>
+      <Route path={`${match.url}/codeofconduct`} render={() => <Container><CodeOfConductFull/></Container>}/>
+    </Background>
   </ThemeProvider>
-)
+))
