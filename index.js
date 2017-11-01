7c56665a29bad661444ffc35e0c6067dcc47e8d3
Glen Maddern <glenmaddern@gmail.com>
2017-11-01 19:29:49 +1100

    i have brought shame upon my ancestors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./src/server')
 const port = process.env.PORT || 3000
 
-server.listen(port, () => console.log('listening on port 3000'))
+server.listen(port, () => console.log(`listening on port ${port}`))
