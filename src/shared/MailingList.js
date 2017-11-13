37a55ec608632baddd9a86b0bc7c90ba3fd57f87
Glen Maddern <glenmaddern@gmail.com>
2017-11-13 18:02:27 +1100

    remove dead import

--- src/shared/MailingList.js
+++ src/shared/MailingList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { withTheme } from 'styled-components'
+import styled from 'styled-components'
 
 import Headline from './components/Headline'
 import { ButtonThemed } from './components/Buttons'
