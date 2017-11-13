d31736cc60aa515bd637bf5eb21a3dc28d5dce75
Glen Maddern <glenmaddern@gmail.com>
2017-11-13 18:02:08 +1100

    much simpler overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,5 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-
 module.exports = function override(config, env) {
-  config = injectBabelPlugin('babel-macros', config);
   const existing_loaders = config.module.rules[1].oneOf
-  let babel_loader = existing_loaders.find(({test}) => test instanceof RegExp && test.exec('ugh.js'))
   existing_loaders.unshift({
     test: /\.md$/,
     use: 'md-to-react',
